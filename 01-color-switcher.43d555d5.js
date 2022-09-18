68a17233276f2b3f39ce43fb0b56716bcfe15659
Pleomax07 <103961834+Pleomax07@users.noreply.github.com>
2022-09-18 18:27:29 +0000

    Deploying to gh-pages from @ Pleomax07/goit-js-hw-09@e002592bec319b5ba2d831b89f2dc601e0b44564 🚀

---
+++ 01-color-switcher.43d555d5.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(){a=setInterval((()=>{d.style.cssText=`background-color: #${Math.floor(16777215*Math.random()).toString(16)};`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(a),e.disabled=!1,t.disabled=!0})),t.disabled=!0;let a=null;
+//# sourceMappingURL=01-color-switcher.43d555d5.js.map
